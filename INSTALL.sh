23868cd857dab6be95021e1b4132f872fa76e3b2
Sergey Ponomarev <stokito@gmail.com>
2023-10-30 00:13:49 +0300

    Add Makefile

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 mkdir -p "$DESTDIR/usr/share/pyrite/"
 cp -r --preserve=mode,timestamps ui "$DESTDIR/usr/share/pyrite/"
+mkdir -p "$DESTDIR/usr/lib/python3/dist-packages/"
 cp -r --preserve=mode,timestamps pyrite "$DESTDIR/usr/lib/python3/dist-packages/"
 
 # Copy desktop shortcut
@@ -8,5 +9,5 @@ mkdir -p "$DESTDIR/usr/share/applications"
 cp --preserve=mode,timestamps pyrite.desktop  "$DESTDIR/usr/share/applications"
 mkdir -p "$DESTDIR/usr/share/icons/hicolor/scalable/apps/"
 cp --preserve=mode,timestamps pyrite.svg "$DESTDIR/usr/share/icons/hicolor/scalable/apps/"
-
+mkdir -p "$DESTDIR/usr/bin/"
 install -m u=rwx,g=rx,o=rx ./pyrite.py "$DESTDIR/usr/bin/pyrite"
